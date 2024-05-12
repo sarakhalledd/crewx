e260bdea2efe2d170ec33db993df7f154b1da818
Sara Khaled <sarakhalledd16@gmail.com>
2024-05-12 20:08:19 +0300

    added registration

---
+++ Register.js
@@ -0,0 +1,8 @@
+const xicon = document.querySelector('.xicon');
+          
+// Add an event listener to handle click events
+xicon.addEventListener('click', function() {
+  // Redirect to home.html
+  window.location.href = 'Home.html';
+});
+
